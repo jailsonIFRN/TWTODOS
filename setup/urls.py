6bc468ae66a185f4a84b4d2fd73ac3935dba27cf
notebook_pessoal <professorjailsoninformatica@gmail.com>
2024-09-23 21:53:00 -0300

    1_Criando a página de listagem de tarefas

--- setup/urls.py
+++ setup/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 
-from todos.views import todo_list
+from todos.views import TodoListView, TodoCreateView
 
 
 
 # todas a rotas existentes para ser executadas - vem do "setup > urls.py"
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", todo_list),
+    path("", TodoListView.as_view()),
+    path("create", TodoCreateView.as_view()),
 
 ]
