47e4790fa7fceb35cff61bc8beb7502e4fa322c8
notebook_pessoal <professorjailsoninformatica@gmail.com>
2024-09-25 07:00:00 -0300

    1_Criando uma página de cadastro de tarefas no Django

--- setup/urls.py
+++ setup/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path
 
 
-from todos.views import todo_list
+from todos.views import TodoListView, TodoCreateView
 
 
 # todas a rotas existentes para ser executadas - vem do "setup > urls.py"
 urlpatterns = [
     path("admin/", admin.site.urls),
-     path("", todo_list),
+    path("", TodoListView.as_view(), name="todo_list"),
+    path("create", TodoCreateView.as_view(), name="todo_create"),
+
 
 ]
