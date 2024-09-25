b55f0a19f7a6de1085816795245d35aa79ccdb53
notebook_pessoal <professorjailsoninformatica@gmail.com>
2024-09-25 11:28:47 -0300

    1_Criando a funcionalidade de exclusão de tarefa no Django

--- setup/urls.py
+++ setup/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 
-from todos.views import TodoListView, TodoCreateView, TodoUpdateView
+from todos.views import TodoListView, TodoCreateView, TodoUpdateView, TodoDeleteView
 
 
 # todas a rotas existentes para ser executadas - vem do "setup > urls.py"
@@ -11,5 +11,7 @@ urlpatterns = [
     path("", TodoListView.as_view(), name="todo_list"),
     path("create", TodoCreateView.as_view(), name="todo_create"),
     path("update/<int:pk>", TodoUpdateView.as_view(), name="todo_update"),
+    path("delete/<int:pk>", TodoDeleteView.as_view(), name="todo_delete"),
+
 ]
 
